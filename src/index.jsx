34e78308e4c0cda34ba4b202e5fcf3164a069d8b
Prashul Jain <jainprashul@gmail.com>
2020-11-05 15:25:47 +0530

    seo-add

--- src/index.jsx
+++ src/index.jsx
@@ -13,7 +13,15 @@ const LandingPage = () => (
     </FirebaseContextProvider>
 )
 
-ReactDOM.render(<LandingPage />, document.getElementById('root'));
+// ReactDOM.render(<LandingPage />, document.getElementById('root'));
+
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<LandingPage />, rootElement);
+} else {
+  ReactDOM.render(<LandingPage />, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
